f16b639cced7812481011950acb29fa143ab7151
Imran <imranansari954830@gamil.com>
2024-11-12 23:34:12 +0530

    state in class base components

--- src/components/News.js
+++ src/components/News.js
@@ -6,10 +6,10 @@ export class News extends Component {
     return (
       <div className='container my-3'>
         <h2>NewMonkey - Top Headlines</h2>
-          {/*use for the row taki item row mein aaye*/}
-          <div className="row"> 
+          <div className="row"> {/*use for the row taki item row mein aaye*/}
               <div className="col-md-4">
-                <NewsItem title='myTitle' description='myDesc' />/{/*aur jo props 'NewsItem.js' mein pass kiya uske variable ko yaha pull karke usme value assign ki*/}
+                {/*aur jo props 'NewsItem.js' mein pass kiya uske variable ko yaha pull karke usme value assign ki*/}
+                <NewsItem title='myTitle' description='myDesc' imageUrl="https://www.aljazeera.com/wp-content/uploads/2024/11/AP24313564688477-1731234736_37d556-1731237963.jpg?resize=1200%2C630&quality=80" newsUrl="ToDo"/>
               </div>
               
               <div className="col-md-4">
