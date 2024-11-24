1c002b89d20b4b2bfb2d7960ec06598cb5f9ec48
Imran <imranansari954830@gamil.com>
2024-11-24 10:44:02 +0530

    Adding_Previous__Next_Buttons_to_populate_NewsItems

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,16 +6,18 @@ export class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl} = this.props;//yaha props mein two variable pass kiye 'title','description' jinme hum props ki form mein value pass karenge aur yaha aur bhi variable pass kar sakte hain
 
     return (
+      <>
       <div className='my-3'>
         <div className ="card" style = {{width: "18rem"}}>
-          <img src = {imageUrl} className='card-img-top' alt='...'/>
+          <img src = {!imageUrl ? "https://www.investors.com/wp-content/uploads/2017/04/stock-wallstreet-shutter.jpg" : imageUrl} className='card-img-top' alt='...'/> {/*agar kisi component mein 'imageurl' nahi hai to ye wala use kar lo barna 'imageUrl' use kar lo*/}
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">Read More</a> {/*yha href mein newUrl ka link dala and target ='blank' kiya isse isse read more par click karne se news open ho jayegi*/}
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a> {/*yha href mein newUrl ka link dala and target ='blank' kiya isse isse read more par click karne se news open ho jayegi*/}
           </div>
         </div>
-      </div>
+        </div>
+        </>
     )
   }
 }
